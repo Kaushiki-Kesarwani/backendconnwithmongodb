5f4e90a8023502ccb9f1b6804926143b8bbc3678
mahak <kaushikikes@gmail.com>
2026-04-27 23:17:17 +0530

    operations

--- mvcpattern/controllers/productcontroller.js
+++ mvcpattern/controllers/productcontroller.js
@@ -5,6 +5,8 @@ const Product = require('../models/productmodel')
 const getProducts = async(req,res)=>{
     try{
         const allProducts =await Product.find();
+
+        res.status(200).json(allProducts)
         if(!allProducts || allProducts.length === 0){
             res.json({
                 message:"There is no Product"
@@ -19,4 +21,37 @@ const getProducts = async(req,res)=>{
     }
 }
 
+
+const createProducts = async(req,res)=>{
+    try{
+        const{name,price,description,category} = req.body;
+        const newproduct = new Product({name,price,description,category});
+        await newproduct.save();
+        res.status(200).json(newproduct)
+
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        });
+    }
+} 
+
+const updateProducts = async(req,res)=>{
+    try{
+        const {id} = res.params;
+         const{name,price,description,category} = req.body;
+         const updateproduct = await Product.findByIdAndUpdate(id,{name,price,description,category}) 
+ res.status(200).json(updateproduct)
+
+
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        });
+    }
+}
+
 module.exports = {getProducts}
+module.exports = {createProducts}
