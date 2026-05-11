9f7e6d69dc50b2f0a35cd05b688aa91a4304dcc7
mahak <kaushikikes@gmail.com>
2026-05-11 10:04:49 +0530

    add en file

--- mvcpattern/routes/productroutes.js
+++ mvcpattern/routes/productroutes.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const {getProducts} = require('../controllers/productcontroller')
+const {createProducts} = require('../controllers/productcontroller') 
+
 router.get('/products',getProducts);
+
+router.put('/products/:id',createProducts);
+
+
 module.exports = router
